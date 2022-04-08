a265b4558c1a7a0ffece9e5ee836033367a0dbb9
dzhamilia <orujeva_jamilya@mail.ru>
2022-04-08 14:27:13 +0300

    changes

--- client/src/components/MainPage/SignUp.js
+++ client/src/components/MainPage/SignUp.js
@@ -2,30 +2,27 @@ import React from 'react';
 
 function SignUp() {
   return (
-    <div className="content user">
-      <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field">
-              <i className="material-icons prefix">account_circle</i>
-              <input id="icon_prefix" type="text" className="validate" />
-              <label htmlFor="icon_prefix">Имя</label>
-            </div>
-            <div className="input-field">
-              <i className="material-icons prefix">alternate_email</i>
-              <input id="icon_telephone" type="tel" className="validate" />
-              <label htmlFor="icon_telephone">email</label>
-            </div>
-            <div className="input-field">
-              <i className="material-icons prefix">key</i>
-              <input id="icon_password" type="password" className="validate" />
-              <label htmlFor="icon_password">Пароль</label>
-            </div>
-            <button className="waves-effect cyan waves-teal btn-flat">зарегистрироваться</button>
+    <center>
+      <div className="reg content">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Имя</label>
+            <input name="login" type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите имя" />
           </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+            <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите почту" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">Пароль</label>
+            <input name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Придумайте пароль" />
+          </div>
+
+          <button type="submit" className="btn btn-warning">Регистрация</button>
         </form>
       </div>
-    </div>
+    </center>
+
   );
 }
 
