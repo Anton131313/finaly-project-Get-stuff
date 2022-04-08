efdf5e046b029a7bd8917de4e6db9584451ce2dc
dzhamilia <orujeva_jamilya@mail.ru>
2022-04-08 16:57:27 +0300

    profile changes

--- client/src/components/MainPage/Footer.js
+++ client/src/components/MainPage/Footer.js
@@ -3,18 +3,20 @@ import style from './MainPage.module.css';
 
 function Footer() {
   return (
-    <nav className={`navbar navbar-expand-lg navbar-success ${style.headercolor}`}>
-      <div className="container1">
-        <footer className="text-center text-lg-start text-black">
-          <div className="text-center p-3">
-            © 2022 Copyright:
-            <p className="text-black" href="/">
-              Служба поддержки: support@getkhlam.ru
-            </p>
-          </div>
-        </footer>
-      </div>
-    </nav>
+    <div className={style.footerdesc}>
+      <nav className={`navbar navbar-expand-lg navbar-success ${style.headercolor}`}>
+        <div className="container1">
+          <footer className="text-center text-lg-start text-black">
+            <div className="text-center">
+              © 2022 Copyright:
+              <p className="text-black" href="/">
+                Служба поддержки: support@getkhlam.ru
+              </p>
+            </div>
+          </footer>
+        </div>
+      </nav>
+    </div>
   );
 }
 
