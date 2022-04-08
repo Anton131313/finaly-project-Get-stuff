6633ba469f2afc10e6ebbf755e31aac78a24c74a
dzhamilia <orujeva_jamilya@mail.ru>
2022-04-08 09:58:51 +0300

    last changes in main page

--- client/src/components/MainPage/Header.js
+++ client/src/components/MainPage/Header.js
@@ -1,10 +1,13 @@
 import {Link} from 'react-router-dom';
+
 const Header = () => {
   return (
   <>
   <nav className="cyan darken-2">
     <div className="nav-wrapper">
       <a href="/" className="brand-logo">GetХлам</a>
+
+
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link className="nav-link active" to="/login">
               Войти</Link></li>
@@ -13,6 +16,7 @@ const Header = () => {
       </ul>
     </div>
   </nav>
+ 
   </>);
 }
 
--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -5,14 +5,9 @@ const MainPage = () => {
   return (
   <>
   <div className="content">
-<<<<<<< HEAD
-    
-
-=======
   <center>
   <Banner />
   </center>
->>>>>>> f70013467a427e6c15c149903632a5a4d95e8e78
   </div>
 
   </>);
