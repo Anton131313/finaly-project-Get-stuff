39ff6eebeb3d6e153cc3d58d6e9debfb1525a33e
Anton <Altair3464@yandex.ru>
2022-04-11 16:58:59 +0300

    наконец то доделали сессии

--- server/src/controllers/addBidding.controller.js
+++ server/src/controllers/addBidding.controller.js
@@ -1,39 +1,36 @@
 const { Bidding, Product, Photo } = require('../../db/models');
 
 const addBidding = async (req, res) => {
-  console.log('<=========session}===========>\n', res.locals);
-  const user_id = 1; // временный хардкод
-  // console.log(`==========>${req.file.originalname}<==========`);
+  // console.log(req.session.user.id);
+  const user_id = req.session.user.id;
 
-  // console.log(req.body);
+  const {
+    title,
+    info,
+    category_id,
+    condition_id,
+    price,
+    price_step,
+    end_bidding,
+    location,
+  } = await req.body;
 
-  // const {
-  //   title,
-  //   info,
-  //   category_id,
-  //   condition_id,
-  //   price,
-  //   price_step,
-  //   end_bidding,
-  //   location,
-  // } = await req.body;
+  const newProduct = await Product.create({
+    title,
+    info,
+    user_id,
+    category_id: Number(category_id),
+    condition_id: Number(condition_id),
+    location,
+    img: req.file.originalname,
+  });
 
-  // const newProduct = await Product.create({
-  //   title,
-  //   info,
-  //   user_id,
-  //   category_id: Number(category_id),
-  //   condition_id: Number(condition_id),
-  //   location,
-  //   img: req.file.originalname,
-  // });
-
-  // const newBidding = await Bidding.create({
-  //   product_id: newProduct.id,
-  //   price,
-  //   price_step,
-  //   end_bidding,
-  // });
+  const newBidding = await Bidding.create({
+    product_id: newProduct.id,
+    price,
+    price_step,
+    end_bidding,
+  });
 
   // const newPhoto = await Photo.create({
   //   product_id: newProduct.id,
