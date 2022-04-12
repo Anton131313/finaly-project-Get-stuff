300c54f523d1b428a1326b8ec7b9672d1c9c757c
Inga Zhamsoeva <injoyme@gmail.com>
2022-04-12 14:46:04 +0300

    added filter

--- client/src/components/Bidding/BiddingList.js
+++ client/src/components/Bidding/BiddingList.js
@@ -11,40 +11,46 @@ function BiddingList() {
     dispatch(getAllProductsFromDB());
     setbuttonState(false);
   }, []);
-  // console.log(999999999999, products);
+  console.log(999999999999, products);
+  const [filter, setFilter] = useState(products);
+  const filterProduct = (category) => {
+    const filteredProducts = products.filter((el) => el['Category.nameCategory'] === category);
+    console.log('filter', filteredProducts);
+    setFilter(filteredProducts);
+  };
+
   return (
-    <div className="my-5 py-5">
-      <div className="row">
-        <div className="col-12 mb-4">
-          <h2 className="display-6 fw-bolder text-center">Рекомендуемое</h2>
+    <div className="my-4 py-2">
+      <div className="row mb-5">
+        <div className="col-12 mb-3">
+          <h3 className="display-6 fw-bolder text-center">Все объявления</h3>
           <hr />
           <div className="buttons d-flex justify-content-center mb-2 pb-2">
-            <button className="btn btn-outline-dark me-2">Все</button>
-            <button className="btn btn-outline-dark me-2">Товары для дома</button>
-            <button className="btn btn-outline-dark me-2">Одежда</button>
-            <button className="btn btn-outline-dark me-2">Автотовары</button>
-            <button className="btn btn-outline-dark me-2">Техника</button>
-            <button className="btn btn-outline-dark me-2">Зоотовары</button>
-            <button className="btn btn-outline-dark me-2">Товары для детей</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => setFilter(products)}>Все</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('Товары для дома')}>Товары для дома</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('Одежда')}>Одежда</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('Автотовары')}>Автотовары</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('Техника')}>Техника</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('Зоотовары')}>Зоотовары</button>
+            <button className="btn btn-outline-dark me-2" onClick={() => filterProduct('Товары для детей')}>Товары для детей</button>
           </div>
         </div>
-      </div>
-      <div className="row justify-content-center">
-        {/* {pages.length && <Pagination />} */}
-        {products?.map((el) => (
-          <BiddingCard
-            key={el.id}
-            id={el.id}
-            img={el.img}
-            title={el.title}
-            info={el.info}
-            condition={el['Condition.nameCondition']}
-            category={el['Category.nameCategory']}
-            location={el.location}
-            buttonState={buttonState}
-          />
-        ))}
-
+        <div className="list">
+          {/* {pages.length && <Pagination />} */}
+          {filter?.map((el) => (
+            <BiddingCard
+              key={el.id}
+              id={el.id}
+              img={el.img}
+              title={el.title}
+              info={el.info}
+              condition={el['Condition.nameCondition']}
+              category={el['Category.nameCategory']}
+              location={el.location}
+              buttonState={buttonState}
+            />
+          ))}
+        </div>
       </div>
 
     </div>
--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -4,7 +4,7 @@ import BiddingList from '../Bidding/BiddingList';
 
 function MainPage() {
   return (
-    <div className="container" style={{ height: '100vh' }}>
+    <div className="container style={{ height: '100vh' }}">
       <center>
         <CarouselPage />
       </center>
