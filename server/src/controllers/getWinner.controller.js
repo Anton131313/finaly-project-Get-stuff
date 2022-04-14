4d2fafc04f57331cdb016fc8bbcbea4148a8adde
Anton <Altair3464@yandex.ru>
2022-04-14 18:38:37 +0300

    ыыыыыыыыыыыыыыыыыыыы

--- server/src/controllers/getWinner.controller.js
+++ server/src/controllers/getWinner.controller.js
@@ -3,34 +3,41 @@ const {
 } = require('../../db/models');
 
 const getWinner = async (req, res) => {
-  console.log('<===================>');
   const product_id = req.params.id;
-  const currBidding = await Bidding.findOne({
-    where: {
-      product_id,
-    },
-    attributes: ['price'],
-  });
-  const winner_Bidding = await User_Bidding.findOne({
-    where: {
-      users_bid: currBidding.price,
-    },
-    attributes: ['user_id'],
-  });
-  const currUser = await User.findOne({
-    where: {
-      id: winner_Bidding.user_id,
-    },
-  });
-  const winner = {
-    user_id: winner_Bidding.user_id,
-    name: currUser.name,
-    phone: currUser.phone,
-    photo: currUser.photo,
-    email: currUser.email,
-    price: currBidding.price,
-  };
-  res.json(winner);
+  try {
+    const currBidding = await Bidding.findOne({
+      where: {
+        product_id,
+      },
+      attributes: ['price'],
+    });
+
+    const winner_Bidding = await User_Bidding.findOne({
+      where: {
+        users_bid: currBidding.price,
+      },
+      attributes: ['user_id'],
+    });
+
+    const currUser = await User.findOne({
+      where: {
+        id: winner_Bidding.user_id,
+      },
+    });
+
+    const winner = {
+      user_id: winner_Bidding.user_id,
+      name: currUser.name,
+      phone: currUser.phone,
+      photo: currUser.photo,
+      email: currUser.email,
+      price: currBidding.price,
+    };
+
+    res.json(winner);
+  } catch (error) {
+    res.sendStatus(418);
+  }
 };
 
 module.exports = { getWinner };
